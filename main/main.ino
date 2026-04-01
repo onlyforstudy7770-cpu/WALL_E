d1da14107c71472420f5a124d8188fc44801cda8
windows_AK <onlyforstudy7770@gmail.com>
2026-04-02 04:30:29 +0530

    fix buxes

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,12 @@
 
 
 Servo myServo;
-const int receiverPin = 39;
+// const int receiverPin = 39;
 const int servoPin = 22;
 
 IBusBM IBus;  // Create IBus Object
+
+
 
 const int rxPin = 36;
 
@@ -39,49 +41,12 @@ const int lead_motor_Bin2 = 15;
 // The speed of sound is roughly 0.034 centimeters per microsecond
 const float SOUND_SPEED = 0.034;
 
-// Control Motor A
-void mControlA(int mspeed ) {
-
-  // Determine direction
-  if (mspeed < 0) {
-    // Motor backward
-    digitalWrite(motor_Ain1, LOW);
-    digitalWrite(motor_Ain2, HIGH);
-  } else {
-    // Motor frward
-    digitalWrite(motor_Ain1, HIGH);
-    digitalWrite(motor_Ain2, LOW);
-  }
-
-  // Control motor
-  analogWrite(motor_PWMA, abs(mspeed));
-
-}
-
-// Control Motor B
-void mControlB(int mspeed) {
-
-  // Determine direction
-  if (mspeed < 0) {
-    // Motor backward
-    digitalWrite(motor_Bin1, LOW);
-    digitalWrite(motor_Bin2, HIGH);
-  } else {
-    // Motor forward
-    digitalWrite(motor_Bin1, HIGH);
-    digitalWrite(motor_Bin2, LOW);
-  }
-
-  // Control motor
-  analogWrite(motor_PWMB, abs(mspeed));
-
-}
 
 
 // Read the number of a given channel and convert to the range provided.
 // If the channel is off, return the default value
 int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue) {
-  uint16_t ch = ibus.readChannel(channelInput);
+  uint16_t ch = IBus.readChannel(channelInput);
   if (ch < 100) return defaultValue;
   return map(ch, 1000, 2000, minLimit, maxLimit);
 }
@@ -93,11 +58,64 @@ bool readSwitch(byte channelInput, bool defaultValue) {
   return (ch > 50);
 }
 
+// Control Motor 
+void move_front(int speed) {
+  digitalWrite(motor_Ain1, HIGH);
+  digitalWrite(motor_Ain2, LOW);
+
+  digitalWrite(motor_Bin1, HIGH);
+  digitalWrite(motor_Bin2, LOW);
+
+  analogWrite(motor_PWMA, speed);
+  analogWrite(motor_PWMB, speed);
+}
+void move_back(int speed) {
+  digitalWrite(motor_Ain1, LOW);
+  digitalWrite(motor_Ain2, HIGH);
+
+  digitalWrite(motor_Bin1, LOW);
+  digitalWrite(motor_Bin2, HIGH);
+
+  analogWrite(motor_PWMA, speed);
+  analogWrite(motor_PWMB, speed);
+}
+void move_left(int speed) {
+  digitalWrite(motor_Ain1, HIGH);
+  digitalWrite(motor_Ain2, LOW);
+
+  digitalWrite(motor_Bin1, LOW);
+  digitalWrite(motor_Bin2, LOW);
+
+  analogWrite(motor_PWMA, speed);
+  analogWrite(motor_PWMB, speed);
+}
+void move_right(int speed) {
+  digitalWrite(motor_Ain1, LOW);
+  digitalWrite(motor_Ain2, LOW);
+
+  digitalWrite(motor_Bin1, HIGH);
+  digitalWrite(motor_Bin2, LOW);
+
+  analogWrite(motor_PWMA, speed);
+  analogWrite(motor_PWMB, speed);
+}
+void stop() {
+  digitalWrite(motor_Ain1, LOW);
+  digitalWrite(motor_Ain2, LOW);
+
+  digitalWrite(motor_Bin1, LOW);
+  digitalWrite(motor_Bin2, LOW);
+
+  analogWrite(motor_PWMA, 0);
+  analogWrite(motor_PWMB, 0);
+}
+
+
 void setup() {
 
   Serial.begin(115200);
   // Start IBus on Serial2 (RX 2)
-  IBus.begin(Serial2);
+  IBus.begin(Serial2 , 0 , rxPin );
 
   pinMode(motor_Ain1, OUTPUT);
   pinMode(motor_Ain2, OUTPUT);
@@ -115,9 +133,6 @@ void setup() {
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
 
-
-
-  pinMode(receiverPin, INPUT);
   // Attach the servo to GPIO 25
   myServo.attach(servoPin);
 
@@ -140,29 +155,30 @@ void loop() {
    // Get RC channel values
   int CH1 = readChannel(0, -255, 255, 0);
   int CH2 = readChannel(1, -255, 255, 0);
-  int CH3 = readChannel(2, 0, 155, 0);
-  int CH5 = readChannel(4, -100, 100, 0);
-  int CH6 = readSwitch(5, false);
+  int CH3 = readChannel(2, 0, 255, 0);
+  bool CH5 = readSwitch(4, false);
+  bool CH6 = readSwitch(5, false);
 
   // Print values to serial monitor for debugging
-  Serial.print("Ch1 = ");
+  Serial.print("CH1 = ");
   Serial.print(CH1);
 
-  Serial.print(" Ch2 = ");
+  Serial.print(" CH2 = ");
   Serial.print(CH2);
 
   Serial.print(" Ch3 = ");
-  Serial.print(rcCH3);
+  Serial.print(CH3);
 
   Serial.print(" Ch5 = ");
-  Serial.print(rcCH5);
+  Serial.print(CH5);
 
   Serial.print(" Ch6 = ");
-  Serial.println(rcCH6);
+  Serial.println(CH6);
 
 
+  bool run =  true ;
   // turn on the automatic stair climbing mode
-  if (ch6 > 1600)
+  if (CH6)
   {
     float distance = distance_u_s_1();
     //  check if stairs is present or not
@@ -176,18 +192,18 @@ void loop() {
 
         distance = distance_u_s_1();
       }
-      float temp_distance = distance;
 
       move_down_front_lead(255);
       move_down_front_lead(255);
-      int time{ 0 };
-      while (temp_distance - distance >= 250) {
-        distance = distance_u_s_1();
-        delay(500);
-          time++;
+
+      int time = millis();
+
+      while (distance_u_s_1() - distance >= 25) {
+        delay(100);
       }
       stop_front_lead();
       stop_back_lead();
+      time = millis()- time;
 
       move_front(125);
       delay(1000);
@@ -195,7 +211,7 @@ void loop() {
       delay(500);
 
       move_up_front_lead(255);
-      delay(500 * time);
+      delay( time);
       stop_front_lead();
       delay(500);
 
@@ -205,7 +221,7 @@ void loop() {
       delay(500);
 
       move_up_back_lead(255);
-      delay(500 * time);
+      delay(time);
       stop_back_lead();
       delay(500);
 
@@ -215,13 +231,13 @@ void loop() {
       delay(500);
     }
   }
-  // turn on stairs climbing down
-  int speed2 = 125;
-  bool run{ true };
-  long height{};
 
-  else if (ch6 < 1400) 
+
+  else if (!CH6) 
   {
+      // turn on stairs climbing down
+  int speed2 = 125;
+  long height = 0 ;
     if (run) {
       height = { distance_u_s_2() };
       digitalWrite(motor_Ain1, HIGH);
@@ -238,10 +254,9 @@ void loop() {
     }
 
     move_back(125);
-    while ((distance_u_s_2() - height < 10)) {
+    while ((distance_u_s_2() - height < 4 )) {
       delay(500);
     }
-    delay(500);
     stop();
 
     move_down_back_lead(255);
@@ -270,36 +285,30 @@ void loop() {
   {
 
     // to move front
-    if (ch2 > 1520 && (ch1 < 1520 || ch1 > 1480)) {
-      int speed = map(ch1, 1520, 2000, 0, 255);
-      move_front(speed);
+    if (CH2 > 10 && (!(CH1 > 10  || CH1 < -10))) {
+      move_front(CH2);
     }
     // to move back
-    else if (ch2 < 1480 && (ch1 < 1520 || ch1 > 1480)) {
-      int speed = map(ch1, 1480, 1000, 0, 255);
-      move_back(speed);
+    else if (CH2 < -10 && (!(CH1 > 10  || CH1 < -10))) {
+      move_back(abs(CH2));
     }
     // to move left
-    else if (ch1 > 1520 && (ch2 < 1520 || ch2 > 1480)) {
-      int speed = map(ch2, 1520, 2000, 0, 255);
-      move_left(speed);
+    else if (CH1 >  10 && (!(CH2 > 10  || CH2 < -10))) {
+     
+      move_left(CH1);
     }
     //to move right
-    else if (ch1 < 1480 && (ch2 < 1520 || ch2 > 1480)) {
-      int speed = map(ch2, 1480, 1000, 0, 255);
-      move_left(speed);
+    else if (CH1 < 1480 &&  (!(CH2 > 10  || CH2 < -10))) {
+      move_left(abs(CH2));
     } else {
       stop();
     }
 
 
-    // Read the PWM pulse width from the receiver
-    unsigned long pwmValue = pulseIn(receiverPin, HIGH, 25000);
-    if (pwmValue > 900 && pwmValue < 2100) 
+    if (CH3 > 10 )
     {
-      int angle = map(pwmValue, 1000, 2000, 0, 90);
-      // Constrain the angle just in case the controller goes slightly out of bounds
-      angle = constrain(angle, 0, 90);
+      int angle = map(CH3, 0, 255, 0, 90);
+      
       // Tell the servo to move to that angle
       myServo.write(angle);
     }
@@ -351,56 +360,7 @@ void stop_lead()
   analogWrite(lead_motor_PWMA, 0);
 
 }
-void move_front(int speed) {
-  digitalWrite(motor_Ain1, HIGH);
-  digitalWrite(motor_Ain2, LOW);
 
-  digitalWrite(motor_Bin1, HIGH);
-  digitalWrite(motor_Bin2, LOW);
-
-  analogWrite(motor_PWMA, speed);
-  analogWrite(motor_PWMB, speed);
-}
-void move_back(int speed) {
-  digitalWrite(motor_Ain1, LOW);
-  digitalWrite(motor_Ain2, HIGH);
-
-  digitalWrite(motor_Bin1, LOW);
-  digitalWrite(motor_Bin2, HIGH);
-
-  analogWrite(motor_PWMA, speed);
-  analogWrite(motor_PWMB, speed);
-}
-void move_left(int speed) {
-  digitalWrite(motor_Ain1, HIGH);
-  digitalWrite(motor_Ain2, LOW);
-
-  digitalWrite(motor_Bin1, LOW);
-  digitalWrite(motor_Bin2, LOW);
-
-  analogWrite(motor_PWMA, speed);
-  analogWrite(motor_PWMB, speed);
-}
-void move_right(int speed) {
-  digitalWrite(motor_Ain1, LOW);
-  digitalWrite(motor_Ain2, LOW);
-
-  digitalWrite(motor_Bin1, HIGH);
-  digitalWrite(motor_Bin2, LOW);
-
-  analogWrite(motor_PWMA, speed);
-  analogWrite(motor_PWMB, speed);
-}
-void stop() {
-  digitalWrite(motor_Ain1, LOW);
-  digitalWrite(motor_Ain2, LOW);
-
-  digitalWrite(motor_Bin1, LOW);
-  digitalWrite(motor_Bin2, LOW);
-
-  analogWrite(motor_PWMA, 0);
-  analogWrite(motor_PWMB, 0);
-}
 float distance_u_s_1() {
 
   digitalWrite(trigPin, LOW);
